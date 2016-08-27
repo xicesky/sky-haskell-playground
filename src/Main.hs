3f1f3a97bff5ccb1314c8c5ae29831dac4c6f427
Markus Dangl <sky@q1cc.net>
2016-08-27 17:42:14 +0200

    Added warnings, added all modules to main.

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,12 @@ module Main where
 -- dummy
 -- might wanna run some tests or something...
 
+-- import everything so stack build actually visits all modules
+import Sky.Lens.SimpleLens
+import Sky.ControlLens.TestControlLens
+import Sky.ControlLens.TestLensCompat hiding (main)
+import Sky.Graph.TestGraph
+
 main :: IO ()
 main = do
   putStrLn "hello world"
