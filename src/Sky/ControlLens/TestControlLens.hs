b2fc1589edb9016fda7f67250a8803382df1d605
Markus Dangl <sky@q1cc.net>
2016-08-31 20:13:18 +0200

    Tested Control.Lens with unnamed ADTs.

--- src/Sky/ControlLens/TestControlLens.hs
+++ src/Sky/ControlLens/TestControlLens.hs
@@ -11,8 +11,41 @@ import Data.Functor.Identity
 
 import Control.Lens
 
--- Basic code generator monad --------------------------------------------------
+-- Usage: ghci -ddump-splices 
+
+-- Some very simple tests for Control.Lens -------------------------------------
 
 data Foo a = Foo { _bar :: Int, _baz :: Int, _quux :: a }
+  deriving (Eq, Show)
 makeLenses ''Foo
+xFoo :: Foo Int
+xFoo = Foo 1 2 3
 
+data MyADT =
+    ADTSingle Int
+  | ADTTuple Bool Int
+  deriving (Eq, Show)
+makeLenses ''MyADT
+xADT :: MyADT
+xADT = ADTTuple True 5
+-- Result: doesn't get us anywhere
+
+-- Make custom lenses for those
+adtInt :: Lens' MyADT Int
+adtInt = lens get set where
+  get :: MyADT -> Int
+  get (ADTSingle i) = i
+  get (ADTTuple _ i) = i
+  set :: MyADT -> Int -> MyADT
+  set (ADTSingle _) j = ADTSingle j
+  set (ADTTuple v _) j = ADTTuple v j
+
+-- This is wrong, it should be a "Setting" or something which my fail
+adtBool :: Lens' MyADT Bool
+adtBool = lens get set where
+  get :: MyADT -> Bool
+  get (ADTSingle _) = error "Total fail"
+  get (ADTTuple v _) = v
+  set :: MyADT -> Bool -> MyADT
+  set (ADTSingle _) _ = error "Total fail"
+  set (ADTTuple _ j) v = ADTTuple v j
