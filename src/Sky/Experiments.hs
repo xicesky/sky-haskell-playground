bfa33531153c07fdfb861ae56ad046887a5c40e6
Markus Dangl <sky@q1cc.net>
2016-09-03 05:46:00 +0200

    Module with dummy types and defs.

---
+++ src/Sky/Experiments.hs
@@ -0,0 +1,33 @@
+
+module Sky.Experiments where
+
+data A
+data B
+data C
+data D
+data S
+data T
+
+data F a    -- Functor TODO
+data G a    -- Cofunctor TODO
+
+data M a    -- Monad
+
+uu = undefined
+
+ab = uu :: A -> B
+
+s_ma = uu :: S -> M A
+b_mt = uu :: B -> M T
+
+instance Functor M where
+    fmap = undefined
+
+instance Applicative M where
+    pure = undefined
+    (<*>) = undefined
+
+instance Monad M where
+    return = undefined
+    (>>=) = undefined
+
