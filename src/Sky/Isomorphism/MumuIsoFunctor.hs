836a742d3c59028afdd7b077a3430a2dbfba54b7
Markus Dangl <markus@q1cc.net>
2016-09-11 18:59:05 +0200

    It works!
    We can make any SemiIsomorphism from a to b into a IsoFunctor
    using Type Families! This is _MUCH MORE USEFUL_ and usable than
    functional dependencies.
    It's much easier to read, too, and the type errors don't look
    just as arcane and aweful.

--- src/Sky/Isomorphism/MumuIsoFunctor.hs
+++ src/Sky/Isomorphism/MumuIsoFunctor.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}          -- IsoFunctorStageOne (MumuIso Maybe x)
 {-# LANGUAGE MultiParamTypeClasses #-}      -- IsoFunctorStageTwo
-{-# LANGUAGE FunctionalDependencies #-}     -- IsoFunctorStageTwo
+{-# LANGUAGE TypeFamilies #-}
+--{-# LANGUAGE FunctionalDependencies #-}     -- IsoFunctorStageTwo
+
 
 {-| Non-polymorphic semi-isomorphisms.
 -}
@@ -20,34 +22,37 @@ import Control.Monad ((<=<))
 
 ----------------------------------------------------------------------------------------------------
 
-class SemiIsomorphism i where
-    packSemiIsomorphism :: Monad m => (a -> m b, b -> m a) -> i m a b
-    unpackSemiIsomorphism :: Monad m => i m a b -> (a -> m b, b -> m a)
+class (Category i, Monad (MonadT i)) => SemiIsomorphism i where
+    type MonadT i :: * -> *
+    --type MonadT i = m
+
+    packSemiIsomorphism :: (a -> (MonadT i) b, b -> (MonadT i) a) -> i a b
+    unpackSemiIsomorphism :: i a b -> (a -> (MonadT i) b, b -> (MonadT i) a)
     -- Derived
-    toSemiIsomorphism :: Monad m => (a -> m b) -> (b -> m a) -> i m a b
+    toSemiIsomorphism :: (a -> (MonadT i) b) -> (b -> (MonadT i) a) -> i a b
     toSemiIsomorphism amb bma = packSemiIsomorphism (amb, bma)
-    applySemiIsomorphism :: Monad m => i m a b -> a -> m b
+    applySemiIsomorphism :: i a b -> a -> (MonadT i) b
     applySemiIsomorphism = fst . unpackSemiIsomorphism
-    unapplySemiIsomorphism :: Monad m => i m a b -> b -> m a
+    unapplySemiIsomorphism :: i a b -> b -> (MonadT i) a
     unapplySemiIsomorphism = snd . unpackSemiIsomorphism
-    revertSemiIsomorphism :: Monad m => i m a b -> i m b a
+    revertSemiIsomorphism :: i a b -> i b a
     revertSemiIsomorphism = packSemiIsomorphism . swap . unpackSemiIsomorphism
-    convertSemiIsomorphism :: (Monad m, SemiIsomorphism j) => j m a b -> i m a b
+    convertSemiIsomorphism :: (SemiIsomorphism j, MonadT j ~ MonadT i) => j a b -> i a b
     convertSemiIsomorphism = packSemiIsomorphism . unpackSemiIsomorphism
 
 ----------------------------------------------------------------------------------------------------
 -- Operators and stuff
 
-iso :: forall i m a b. (SemiIsomorphism i, Monad m) => (a -> m b) -> (b -> m a) -> i m a b
+iso :: forall i a b. (SemiIsomorphism i) => (a -> (MonadT i) b) -> (b -> (MonadT i) a) -> i a b
 iso = toSemiIsomorphism
 
-apply :: forall i m a b. (SemiIsomorphism i, Monad m) => i m a b -> a -> m b
+apply :: forall i a b. (SemiIsomorphism i) => i a b -> a -> (MonadT i) b
 apply = applySemiIsomorphism
 
-unapply :: forall i m a b. (SemiIsomorphism i, Monad m) => i m a b -> b -> m a
+unapply :: forall i a b. (SemiIsomorphism i) => i a b -> b -> (MonadT i) a
 unapply = unapplySemiIsomorphism
 
-convert :: forall i j m a b. (SemiIsomorphism i, Monad m, SemiIsomorphism j) => i m a b -> j m a b
+convert :: forall i j a b. (SemiIsomorphism i, SemiIsomorphism j, MonadT i ~ MonadT j) => i a b -> j a b
 convert = convertSemiIsomorphism
 
 ----------------------------------------------------------------------------------------------------
@@ -60,11 +65,13 @@ instance Monad m => Category (MumuIso m) where
     (MumuIso (applyF, unapplyF)) . (MumuIso (applyG, unapplyG)) =
         MumuIso ((applyF <=< applyG), (unapplyG <=< unapplyF))
 
-instance SemiIsomorphism MumuIso where
+instance (Monad m) => SemiIsomorphism (MumuIso m) where
+    type MonadT (MumuIso m) = m
     packSemiIsomorphism = MumuIso
     unpackSemiIsomorphism = _rawMumuIso
 
 ----------------------------------------------------------------------------------------------------
+-- Semi-Isomorphisms can be used as IsoFunctors!
 
 class IsoFunctorStageOne f where
     isomap1 :: forall a b. MumuIso Maybe a b -> f a -> f b
@@ -74,11 +81,41 @@ instance IsoFunctorStageOne (MumuIso Maybe x) where
     isomap1 ab xa = ab . xa
 
 ----------------------------------------------------------------------------------------------------
-
 -- But we'd like to make it work with any monad m
-class IsoFunctorStageTwo f m | f -> m where
-    isomap2 :: forall a b. MumuIso m a b -> f a -> f b
 
-instance (Monad m) => IsoFunctorStageTwo (MumuIso m x) m where
+class (Monad (MonadStageTwo f)) => IsoFunctorStageTwo f where
+    type MonadStageTwo f :: * -> *
+    isomap2 :: forall a b. MumuIso (MonadStageTwo f) a b -> f a -> f b
+
+instance (Monad m) => IsoFunctorStageTwo (MumuIso m x) where
+    type MonadStageTwo (MumuIso m x) = m
+
     isomap2 :: forall a b. MumuIso m a b -> MumuIso m x a -> MumuIso m x b
     isomap2 ab xa = ab . xa
+
+----------------------------------------------------------------------------------------------------
+-- Now make it work for _ANY SEMIISOMORPHISM_
+
+class (Monad (MonadStageThree f)) => IsoFunctorStageThree f where
+    type MonadStageThree f :: * -> *
+    isomap3 :: forall a b i. (SemiIsomorphism i, MonadT i ~ MonadStageThree f) => i a b -> f a -> f b
+
+instance (Monad m) => IsoFunctorStageThree (MumuIso m x) where
+    type MonadStageThree (MumuIso m x) = m
+
+    isomap3 :: forall a b i. (SemiIsomorphism i, MonadT i ~ m) => i a b -> MumuIso m x a -> MumuIso m x b
+    isomap3 ab xa = (convert ab) . xa
+
+----------------------------------------------------------------------------------------------------
+-- Can we make it work with _ANY 2 SEMIISOMORPHISMS_ ?
+
+-- Same as IsoFunctorStageThree, redefined to avoid conflicts
+class IsoFunctorStageFour f where
+    type MonadStageFour f :: * -> *
+    isomap4 :: forall a b i. (SemiIsomorphism i, MonadT i ~ MonadStageFour f) => i a b -> f a -> f b
+
+instance (SemiIsomorphism j) => IsoFunctorStageFour (j x) where
+    type MonadStageFour (j x) = MonadT j
+
+    isomap4 :: forall a b i. (SemiIsomorphism i, MonadT i ~ MonadT j) => i a b -> j x a -> j x b
+    isomap4 ab xa = (convert ab) . xa
