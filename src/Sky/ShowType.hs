ddee30900b1c795801f83759abadfd5d9fb5ca79
Markus Dangl <markus@q1cc.net>
2016-09-11 18:18:01 +0200

    Show types of arbitrary arguments...

---
+++ src/Sky/ShowType.hs
@@ -0,0 +1,27 @@
+
+{-# LANGUAGE InstanceSigs #-}               -- Because i love it
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Sky.ShowType where
+
+-- Shows type of an argument
+-- Does not work for polymorphic types of course!
+
+class ShowType t where
+    showType :: t -> String
+    printType :: t -> IO ()
+    printType = putStrLn . showType
+
+instance ShowType () where
+    showType _ = "()"
+
+instance ShowType Bool where
+    showType _ = "Bool"
+
+instance ShowType Int where
+    showType _ = "Int"
+
+instance (ShowType a, ShowType b) => ShowType (a -> b) where
+    showType _ = showType a ++ " -> " ++ showType b where
+        a = undefined :: a
+        b = undefined :: b
