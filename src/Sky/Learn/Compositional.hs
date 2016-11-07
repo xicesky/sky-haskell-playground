8e30671f5962b784a11fe12c79b10beeca7f2b62
Markus Dangl <markus@q1cc.net>
2016-11-07 17:30:01 +0100

    Decoupling recursion.

--- src/Sky/Learn/Compositional.hs
+++ src/Sky/Learn/Compositional.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE InstanceSigs           #-}
 {-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE UndecidableInstances   #-}
 
 {- | Compositional data types
 See http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.302.6303&rep=rep1&type=pdf
@@ -13,38 +14,61 @@ And https://hackage.haskell.org/package/compdata
 module Sky.Learn.Compositional where
 
 ----------------------------------------------------------------------------------------------------
+-- Essentially Data.Fix
 
-data Exp
+-- Initial f-Algebra
+data Term f = Term (f (Term f))
+
+instance Show (f (Term f)) => Show (Term f) where
+    show (Term x) = "(" ++ show x ++ ")"
+  
+instance Eq (f (Term f)) => Eq (Term f) where
+    (Term a) == (Term b) = a == b
+
+----------------------------------------------------------------------------------------------------
+
+data ExpS r
     = Const Int
-    | Pair  Exp Exp
-    | Mult  Exp Exp
-    | Fst   Exp
+    | Pair  r r
+    | Mult  r r
+    | Fst   r
     deriving (Eq, Show)
 
-data Value
+data ValueS r
     = VConst Int
-    | VPair Value Value
+    | VPair r r
     deriving (Eq, Show)
+
+type Exp = Term ExpS
+type Value = Term ValueS
+
+const' x    = Term $ Const x
+pair' x y   = Term $ Pair x y
+mult' x y   = Term $ Mult x y
+fst' x      = Term $ Fst x
+
+vconst' x   = Term $ VConst x
+vpair' x y  = Term $ VPair x y
 
 ----------------------------------------------------------------------------------------------------
 -- Algebra
 
 eval :: Exp -> Value
-eval (Const n)  = VConst n
-eval (Pair x y) = VPair (eval x) (eval y)
-eval (Mult x y) = let       -- Problem: Non-exhaustive pattern -> GADTs?
-    VConst m    = eval x
-    VConst n    = eval y
-    in VConst (m * n)
-eval (Fst p)    = let
-    VPair x y   = eval p
+eval (Term (Const n))   = vconst' n
+eval (Term (Pair x y))  = vpair' (eval x) (eval y)
+eval (Term (Mult x y))  = let
+    (Term (VConst m))   = eval x
+    (Term (VConst n))   = eval y
+    in vconst' (m * n)
+eval (Term (Fst p))     = let
+    (Term (VPair x y))  = eval p
     in x
 
 ----------------------------------------------------------------------------------------------------
 -- Example
 
 expExample :: Exp
-expExample = (Fst (Pair (Const 2) (Const 3))) `Mult` (Const 5)
+expExample = (fst' (pair' (const' 2) (const' 3))) `mult'` (const' 5)
 
 main :: IO ()
 main = print $ eval expExample
