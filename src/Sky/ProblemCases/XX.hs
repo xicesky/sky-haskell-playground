56ae62aad7c686e9beebd5edb426f8d126924f3a
Markus Dangl <markus@q1cc.net>
2016-11-16 04:34:46 +0100

    Added problem cases with notes
    Type inference making problems again... Sometimes unification
    can just be too hard to understand.

---
+++ src/Sky/ProblemCases/XX.hs
@@ -0,0 +1,22 @@
+
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE RankNTypes             #-}
+
+module XX where
+
+g :: (forall a. a) -> b
+g x = undefined
+
+--f = \x -> g x         -- Error
+
+{- Problem:
+Couldn't match expected type `a' with actual type `t' because type variable `a' would escape its scope
+      This (rigid, skolem) type variable is bound by ...
+-}
+
+-- Fix by specifying the type for x:
+f = \(x :: forall a. a) -> g x
+
+-- Or for f in this case:
+--f :: (forall a. a) -> b
+--f = \x -> g x
